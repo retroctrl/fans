e4b870ed653ede86a3eff313b22ad547668047ae
Jake Swensen <jake@swensen.io>
2025-01-25 15:12:35 -0600

    fix: make crate no_std

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+#![no_std]
+
 /// Drive method for a fan.
 ///
 /// A fan can be controlled by either setting the duty cycle or a target RPM.
