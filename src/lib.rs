e9d338405a5935e31f1ce14b8acab5589f70dd97
Jake Swensen <jake@swensen.io>
2025-01-25 15:26:54 -0600

    feat: add postcard support

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,17 @@
 
 #![no_std]
 
+#[cfg(feature = "postcard")]
+use postcard_schema::Schema;
+
+#[cfg(feature = "postcard")]
+use serde::{Deserialize, Serialize};
+
 /// Drive method for a fan.
 ///
 /// A fan can be controlled by either setting the duty cycle or a target RPM.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[cfg_attr(feature = "postcard", derive(Deserialize, Serialize, Schema))]
 pub enum FanControl {
     /// Duty cycle for a fan to be driven at.
     DutyCycle(u8),
@@ -21,6 +28,7 @@ pub enum FanControl {
 
 /// Generic mechanism to select a fan.
 #[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "postcard", derive(Deserialize, Serialize, Schema))]
 pub struct FanSelect(pub u8);
 
 /// Type alias for a fan duty cycle.
@@ -31,6 +39,7 @@ pub type FanRpm = u16;
 
 /// A simple report for a fan containing the duty cycle and RPM.
 #[derive(Clone, Copy, Debug, PartialEq)]
+#[cfg_attr(feature = "postcard", derive(Deserialize, Serialize, Schema))]
 pub struct FanReport {
     /// Selected fan that this report belongs to.
     pub select: FanSelect,
